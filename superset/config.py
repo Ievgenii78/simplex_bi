4e81ccf59c0d447baf6312115c3ae639620c0028
root <root@logo-2.europe-west4-a.c.simplex-393108.internal>
2023-09-11 10:48:08 +0000

    before applying  PUBLIC_ROLE_LIKE_GAMMA = True

--- superset/config.py
+++ superset/config.py
@@ -353,7 +353,7 @@ PUBLIC_ROLE_LIKE: Optional[str] = None
 # Babel config for translations
 # ---------------------------------------------------
 # Setup default language
-BABEL_DEFAULT_LOCALE = "en"
+BABEL_DEFAULT_LOCALE = "ru"
 # Your application default translation path
 BABEL_DEFAULT_FOLDER = "superset/translations"
 # The allowed translation for you app
