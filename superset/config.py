6e779fa9a05a31b570de7b4d9d97d2210bbe263d
root <root@logo-1.europe-west4-a.c.simplex-393108.internal>
2023-09-26 15:52:46 +0000

    new ap name datapunkt

--- superset/config.py
+++ superset/config.py
@@ -281,7 +281,7 @@ SCHEDULED_QUERIES: Dict[str, Any] = {}
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "SimpleX"
+APP_NAME = "Datapunkt"
 
 # Specify the App icon
 APP_ICON = "/static/assets/images/superset-logo-horiz.png"
